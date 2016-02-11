411973871187107fd6dcb23fcd18d8406cb3273d
Tyler Hall <hal12062@byui.edu>
2016-02-10 17:17:42 -0700

    search

--- BlocSpot/ViewController.swift
+++ BlocSpot/ViewController.swift
@@ -9,41 +9,65 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
     
     
-    var mapView: MKMapView!
+    let mapView = MKMapView()
+    let searchBar = UISearchBar()
+    var mapKitItems = [MKMapItem]()
+    
+    
+    //MKMapViewDelegate
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //create a mapView
-        mapView = MKMapView()
         
         //set vie to mapView
-        view = mapView
+        self.view.addSubview(mapView)
+        self.mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let topMapViewConstraint = NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: self.mapView, attribute: .Top, multiplier: 1.0, constant: 0.0)
+        let bottomMapViewConstraint = NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: self.mapView, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let leadingMapViewConstraint = NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: self.mapView, attribute: .Leading, multiplier: 1.0, constant: 0.0)
+        let trailingMapviewConstraint = NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: self.mapView, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
+        
+        for constraint in [topMapViewConstraint, bottomMapViewConstraint, leadingMapViewConstraint, trailingMapviewConstraint]{
+            constraint.active = true
+        }
+        
+        //add text input box for map query
+        
+        //mapView.delegate = self
+        searchBar.delegate = self
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search"
+        searchBar.searchBarStyle = .Minimal
+        searchBar.becomeFirstResponder()
+        
+        view.addSubview(searchBar)
         
         //segmented views for different types of maps
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+//        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+//        
+
+//        
+//        segmentedControl.selectedSegmentIndex = 0
+//        
+//        segmentedControl.addTarget(self, action: "mapTypeChanged:", forControlEvents: .ValueChanged)
+//        view.addSubview(segmentedControl)
         
-        segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        //layout constraints for the search bar, anchor to view
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
         
-        segmentedControl.selectedSegmentIndex = 0
-        
-        segmentedControl.addTarget(self, action: "mapTypeChanged:", forControlEvents: .ValueChanged)
-        
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(segmentedControl)
-        
-        //layout constraints for the seg control, anchor to view
-        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+        let topConstraint = searchBar.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
         
         let margins = view.layoutMarginsGuide
         
-        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let leadingConstraint = searchBar.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
         
-        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        let trailingConstraint = searchBar.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
         
         //activate constraints
         topConstraint.active = true
@@ -53,31 +77,59 @@ class ViewController: UIViewController {
         trailingConstraint.active = true
         
         // initial location set to Lehi, UT
-        let initialLocation = CLLocation(latitude: 40.57, longitude: -111.75)
+//        let initialLocation = CLLocation(latitude: 40.4, longitude: -111.75)
         
         
-         centerMapOnLocation(initialLocation)
+//         centerMapOnLocation(initialLocation)
     }
-    let regionRadius: CLLocationDistance = 50000
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-            regionRadius * 2.0, regionRadius * 2.0)
-        mapView.setRegion(coordinateRegion, animated: true)
-    }
+    let regionRadius: CLLocationDistance = 1000
+//    func centerMapOnLocation(location: CLLocation) {
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+//            regionRadius * 2.0, regionRadius * 2.0)
+//        mapView.setRegion(coordinateRegion, animated: true)
+//    }
     
-    func mapTypeChanged(segControl: UISegmentedControl){
-        switch segControl.selectedSegmentIndex {
-        case 0:
-            mapView.mapType = .Standard
-        case 1:
-            mapView.mapType = .Hybrid
-        case 2:
-            mapView.mapType = .Satellite
-        default:
-            break
+//    func mapTypeChanged(segControl: UISegmentedControl){
+//        switch segControl.selectedSegmentIndex {
+//        case 0:
+//            mapView.mapType = .Standard
+//        case 1:
+//            mapView.mapType = .Hybrid
+//        case 2:
+//            mapView.mapType = .Satellite
+//        default:
+//            break
+//        }
+//    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar)
+    {
+    searchBar.resignFirstResponder()
+    // Do the search...
+        
+    let request = MKLocalSearchRequest()
+    request.naturalLanguageQuery = searchBar.text
+    let search = MKLocalSearch(request: request)
+        
+    search.startWithCompletionHandler { (response, error) -> Void in
+        if error == nil{
+            
+            if let responseVerified = response {
+                self.mapKitItems = responseVerified.mapItems
+                for item in self.mapKitItems{
+                    NSLog(item.name!)
+                    
+                }
+                
+            }
+            
+            
+        } else{
+            // TODO: Handle error
         }
+        }
+    
     }
-
     
 }
 
